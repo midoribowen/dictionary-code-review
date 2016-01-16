3fb45529c9d9d3749db6d0656bd66fa0835a828c
Midori Bowen <midori.p.bowen>
2016-01-15 16:29:35 -0800

    Edit AppTest.java.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -22,70 +22,56 @@ public class AppTest extends FluentTest {
     assertThat(pageSource()).contains("Your Dictionary");
   }
 
-  // @Test
-  // public void wordIsCreatedTest() {
-  //   goTo("http://localhost:4567/");
-  //   click("a", withText("Add a New Word"));
-  //   fill("#word").with("limn");
-  //   submit(".btn");
-  //   assertThat(pageSource()).contains("Your word has been saved");
-  // }
-  //
-  // @Test
-  // public void wordIsDisplayedTest() {
-  //   goTo("http://localhost:4567/words/new");
-  //   fill("#word").with("limn");
-  //   submit(".btn");
-  //   click("a", withText("View Your Dictionary"));
-  //   assertThat(pageSource()).contains("limn");
-  // }
-  //
-  // @Test
-  // public void wordIsDisplayedOnWordPage() {
-  //   goTo("http://localhost:4567/words/new");
-  //   fill("#word").with("limn");
-  //   submit(".btn");
-  //   click("a", withText("View Your Dictionary"));
-  //   click("a", withText("limn"));
-  //   assertThat(pageSource()).contains("limn");
-  // }
-  //
-  // @Test
-  // public void wordDefinitionFormIsDisplayed() {
-  //   goTo("http://localhost:4567/words/new");
-  //   fill("#word").with("brogue");
-  //   submit(".btn");
-  //   click("a", withText("View Your Dictionary"));
-  //   click("a", withText("brogue"));
-  //   click("a", withText("Add a New Definition"));
-  //   assertThat(pageSource()).contains("Add a Definition to brogue");
-  // }
-
-  // @Test
-  // public void definitionIsAddedAndDisplayed() {
-  //   goTo("http://localhost:4567/words/new");
-  //   fill("#word").with("chirography");
-  //   submit(".btn");
-  //   click("a", withText("View Your Dictionary"));
-  //   click("a", withText("chirography"));
-  //   click("a", withText("Add a New Definition"));
-  //   fill("#definition").with("handwriting, penmanship");
-  //   submit(".btn");
-  //   assertThat(pageSource()).contains("handwriting, penmanship");
-  // }
+  @Test
+  public void wordIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Add a New Word"));
+    fill("#word").with("limn");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your word has been saved");
+  }
 
   @Test
-  public void displayDefinitionsOnAllDefinitionsPage() {
+  public void wordIsDisplayedTest() {
     goTo("http://localhost:4567/words/new");
-    fill("#word").with("mugwump");
+    fill("#word").with("limn");
     submit(".btn");
     click("a", withText("View Your Dictionary"));
-    click("a", withText("mugwump"));
-    click("a", withText("Add a New Definition"));
-    fill("#definition").with("a bolter from the Republican party in 1884");
+    assertThat(pageSource()).contains("limn");
+  }
+
+  @Test
+  public void wordIsDisplayedOnWordPage() {
+    goTo("http://localhost:4567/words/new");
+    fill("#word").with("limn");
     submit(".btn");
-    click("a", withText("View All Definitions"));
-    assertThat(pageSource()).contains("a bolter from the Republican party in 1884");
+    click("a", withText("View Your Dictionary"));
+    click("a", withText("limn"));
+    assertThat(pageSource()).contains("limn");
+  }
+
+  @Test
+  public void wordDefinitionFormIsDisplayed() {
+    goTo("http://localhost:4567/words/new");
+    fill("#word").with("brogue");
+    submit(".btn");
+    click("a", withText("View Your Dictionary"));
+    click("a", withText("brogue"));
+    click("a", withText("Add a New Definition"));
+    assertThat(pageSource()).contains("Add a Definition to brogue");
+  }
+
+  @Test
+  public void definitionIsAddedAndDisplayed() {
+    goTo("http://localhost:4567/words/new");
+    fill("#word").with("chirography");
+    submit(".btn");
+    click("a", withText("View Your Dictionary"));
+    click("a", withText("chirography"));
+    click("a", withText("Add a New Definition"));
+    fill("#definition").with("handwriting, penmanship");
+    submit(".btn");
+    assertThat(pageSource()).contains("handwriting, penmanship");
   }
 
 }
